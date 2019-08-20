e593455e6bef6e2f3b0a789930cbd87e725ad36d
Brad Mortensen <b.mort86@gmail.com>
2019-08-20 16:30:20 -0600

    Monday -- commits

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers
+      resources :customers do
+        resources :orders, only: [:index, :create]
+      end
+      resources :orders, only: [:index, :show] do
+        post :ship, on: :member
+        resources :products, only: [:index, :create]
+      end
       resources :products, only: [:index, :show]
     end
   end
